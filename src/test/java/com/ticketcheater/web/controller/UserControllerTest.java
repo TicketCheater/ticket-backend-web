9dcc6436d875b66cf9fbfed643ccc373a1bc8f49
cpwoo <world9969@khu.ac.kr>
2024-02-23 20:53:36 +0900

    Test: 회원 관련 테스트 작성 (#1)
    Mock 을 이용하여 회원 관련 단위 테스트를 작성했다.

---
+++ src/test/java/com/ticketcheater/web/controller/UserControllerTest.java
@@ -0,0 +1,189 @@
+package com.ticketcheater.web.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ticketcheater.web.controller.request.UserLoginRequest;
+import com.ticketcheater.web.controller.request.UserReissueRequest;
+import com.ticketcheater.web.controller.request.UserSignupRequest;
+import com.ticketcheater.web.dto.UserDTO;
+import com.ticketcheater.web.exception.ErrorCode;
+import com.ticketcheater.web.exception.TicketApplicationException;
+import com.ticketcheater.web.jwt.JwtTokenProvider;
+import com.ticketcheater.web.jwt.TokenDTO;
+import com.ticketcheater.web.service.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    UserService userService;
+
+    @MockBean
+    JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @DisplayName("회원가입이 정상동작한다")
+    @Test
+    @WithAnonymousUser
+    void givenUser_whenSaving_thenSavesUser() throws Exception {
+        String username = "username";
+        String password = "password";
+        String email = "email";
+        String nickname = "nickname";
+
+        when(userService.signup(username, password, email, nickname)).thenReturn(mock(UserDTO.class));
+
+        mvc.perform(post("/users/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new UserSignupRequest("username", "password", "email", "nickname"))))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("중복된 회원정보를 입력시 회원가입은 오류를 내뱉는다")
+    @Test
+    @WithAnonymousUser
+    void givenDuplicatedUser_whenSaving_thenThrowsError() throws Exception {
+        String username = "username";
+        String password = "password";
+        String email = "email";
+        String nickname = "nickname";
+
+        when(userService.signup(username, password, email, nickname)).thenThrow(new TicketApplicationException(ErrorCode.DUPLICATED_USERNAME));
+
+        mvc.perform(post("/users/signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(new UserSignupRequest("username", "password", "email", "nickname"))))
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.DUPLICATED_USERNAME.getStatus().value()));
+    }
+
+    @DisplayName("로그인이 정상동작한다")
+    @Test
+    @WithAnonymousUser
+    void givenUser_whenLogin_thenReceivesToken() throws Exception {
+        String username = "username";
+        String password = "password";
+
+        when(userService.login(username, password)).thenReturn(mock(TokenDTO.class));
+
+        mvc.perform(post("/users/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(new UserLoginRequest("username", "password"))))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("존재하지 않는 유저가 로그인 시 오류를 내뱉는다")
+    @Test
+    @WithAnonymousUser
+    void givenNotFoundUser_whenLogin_thenThrowsError() throws Exception {
+        String username = "username";
+        String password = "password";
+
+        when(userService.login(username, password)).thenThrow(new TicketApplicationException(ErrorCode.USER_NOT_FOUND));
+
+        mvc.perform(post("/users/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(new UserLoginRequest("username", "password"))))
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.USER_NOT_FOUND.getStatus().value()));
+    }
+
+    @DisplayName("다른 비밀번호를 입력한 유저가 로그인 시 오류를 내뱉는다")
+    @Test
+    @WithAnonymousUser
+    void givenWrongPasswordUser_whenLogin_thenThrowsError() throws Exception {
+        String username = "username";
+        String password = "password";
+
+        when(userService.login(username, password)).thenThrow(new TicketApplicationException(ErrorCode.INVALID_PASSWORD));
+
+        mvc.perform(post("/users/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(new UserLoginRequest("username", "password"))))
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.INVALID_PASSWORD.getStatus().value()));
+    }
+
+    @DisplayName("Refresh 토큰을 입력하면 Access 토큰을 재발급한다")
+    @Test
+    @WithMockUser
+    void givenRefreshToken_whenReissue_thenReceivesToken() throws Exception {
+        String username = "username";
+        String refreshToken = "refresh_token";
+
+        when(jwtTokenProvider.reissueAccessToken(username, refreshToken)).thenReturn("new_token");
+
+        mvc.perform(post("/users/reissue")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(new UserReissueRequest("refresh_token"))))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("인증되지 않은 사용자가 Access 토큰 재발급을 요청하면 에러를 내뱉는다")
+    @Test
+    @WithAnonymousUser
+    void givenUnauthorizedUser_whenReissue_thenThrowsError() throws Exception {
+        mvc.perform(post("/users/reissue")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new UserReissueRequest("refresh_token"))))
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.INVALID_TOKEN.getStatus().value()));
+    }
+
+    @DisplayName("만료된 Refresh 토큰을 입력하면 Access 토큰을 재발급하지 않는다")
+    @Test
+    @WithMockUser
+    void givenExpiredRefreshToken_whenReissue_thenThrowsError() throws Exception {
+        doThrow(new TicketApplicationException(ErrorCode.EXPIRED_TOKEN)).when(jwtTokenProvider).reissueAccessToken(any(), eq("refresh_token"));
+        mvc.perform(post("/users/reissue")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(new UserReissueRequest("refresh_token"))))
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.EXPIRED_TOKEN.getStatus().value()));
+    }
+
+    @DisplayName("로그아웃이 정상동작한다")
+    @Test
+    @WithMockUser
+    void givenAuthorizedUser_whenLogout_thenDeletesToken() throws Exception {
+        mvc.perform(post("/users/logout")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("인증되지 않은 사용자가 로그아웃을 요청하면 에러를 내뱉는다")
+    @Test
+    @WithAnonymousUser
+    void givenUnauthorizedUser_whenLogout_thenThrowsError() throws Exception {
+        mvc.perform(post("/users/logout")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.INVALID_TOKEN.getStatus().value()));
+    }
+
+}
---
+++ src/test/java/com/ticketcheater/web/service/UserServiceTest.java
@@ -0,0 +1,131 @@
+package com.ticketcheater.web.service;
+
+import com.ticketcheater.web.config.TestContainerConfig;
+import com.ticketcheater.web.exception.ErrorCode;
+import com.ticketcheater.web.exception.TicketApplicationException;
+import com.ticketcheater.web.fixture.UserFixture;
+import com.ticketcheater.web.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(TestContainerConfig.class)
+@SpringBootTest
+class UserServiceTest {
+
+    @Autowired
+    UserService sut;
+
+    @MockBean
+    UserRepository userRepository;
+
+    @MockBean
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @DisplayName("회원가입이 정상동작한다")
+    @Test
+    void givenUser_whenSaving_thenSavesUser() {
+        String username = "username";
+        String password = "password";
+        String email = "email";
+        String nickname = "nickname";
+
+        when(userRepository.findByUsername(username)).thenReturn(Optional.empty());
+        when(bCryptPasswordEncoder.encode(password)).thenReturn("password_encrypt");
+        when(userRepository.save(any()))
+                .thenReturn(UserFixture.get(
+                        username,
+                        "password_encrypt",
+                        email,
+                        nickname
+                ));
+
+        Assertions.assertDoesNotThrow(() -> sut.signup(username, password, email, nickname));
+    }
+
+    @DisplayName("중복된 회원정보를 입력시 회원가입은 오류를 내뱉는다")
+    @Test
+    void givenDuplicatedUser_whenSaving_thenThrowsError() {
+        String username = "username";
+        String password = "password";
+        String email = "email";
+        String nickname = "nickname";
+
+        when(userRepository.findByUsername(username))
+                .thenReturn(Optional.of(UserFixture.get(username, password, email, nickname)));
+
+        TicketApplicationException exception = Assertions.assertThrows(TicketApplicationException.class,
+                () -> sut.signup(username, password, email, nickname));
+
+        Assertions.assertEquals(ErrorCode.DUPLICATED_USERNAME, exception.getCode());
+    }
+
+    @DisplayName("로그인이 정상동작한다")
+    @Test
+    void givenUser_whenLogin_thenReceivesToken() {
+        String username = "username";
+        String password = "password";
+        String email = "email";
+        String nickname = "nickname";
+
+        when(userRepository.findByUsername(username))
+                .thenReturn(Optional.of(UserFixture.get(
+                        username,
+                        "password",
+                        email,
+                        nickname
+                )));
+        when(bCryptPasswordEncoder.matches(password, "password")).thenReturn(true);
+
+        Assertions.assertDoesNotThrow(() -> sut.login(username, password));
+    }
+
+    @DisplayName("존재하지 않는 유저가 로그인 시 오류를 내뱉는다")
+    @Test
+    void givenNotFoundUser_whenLogin_thenThrowsError() {
+        String username = "username";
+        String password = "password";
+
+        when(userRepository.findByUsername(username)).thenReturn(Optional.empty());
+
+        TicketApplicationException exception = Assertions.assertThrows(TicketApplicationException.class,
+                () -> sut.login(username, password));
+
+        Assertions.assertEquals(ErrorCode.USER_NOT_FOUND, exception.getCode());
+    }
+
+    @DisplayName("다른 비밀번호를 입력한 유저가 로그인 시 오류를 내뱉는다")
+    @Test
+    void givenWrongPasswordUser_whenLogin_thenThrowsError() {
+        String username = "username";
+        String password = "password";
+        String wrongPassword = "password1";
+        String email = "email";
+        String nickname = "nickname";
+
+        when(userRepository.findByUsername(username))
+                .thenReturn(Optional.of(UserFixture.get(
+                        username,
+                        wrongPassword,
+                        email,
+                        nickname
+                )));
+        when(bCryptPasswordEncoder.matches(password, wrongPassword)).thenReturn(false);
+
+        TicketApplicationException exception = Assertions.assertThrows(TicketApplicationException.class,
+                () -> sut.login(username, password));
+
+        Assertions.assertEquals(ErrorCode.INVALID_PASSWORD, exception.getCode());
+    }
+
+}
